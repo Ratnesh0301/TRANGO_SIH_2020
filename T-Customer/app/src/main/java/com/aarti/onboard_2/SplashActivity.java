4bdada655dffef37e7e039420cf05438b73d86e6
arti <arti@engineer.com>
2020-08-01 11:06:34 +0530

    update 1

--- T-Customer/app/src/main/java/com/aarti/onboard_2/SplashActivity.java
+++ T-Customer/app/src/main/java/com/aarti/onboard_2/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         hd.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(SplashActivity.this, MainActivity.class);
+                Intent i=new Intent(SplashActivity.this, Login.class);
                 startActivity(i);
                 finish();
             }
